d64b6c81a065bb351fb87c02bed7b30afbfbd76c
parishilan <parishilanrayamajhi@gmail.com>
2020-10-05 01:10:03 +0545

    console log req body

--- server/modules/main.js
+++ server/modules/main.js
@@ -5,26 +5,31 @@
     const erc = require('./erc20');
 
     const wrapper = async (req) => {
-        const post_data = req.body;
-        console.log('>>>>>>>>>>>>coinase request \n\n ',post_data);
-        const payment_detail = post_data.event.data.payments[0];
-        const detail = payment_detail.value.crypto;
-        const crypto = detail.currency;
-        const amount = detail.amount;
-        console.log(">>>>>>>>>>>>>>>>>>> transction Received <<<<<<<<<<<< :: ", amount, crypto);
-        if (crypto == 'ETH'){
-            eth.forwardEth()
+        try{
+            const post_data = req.body;
+            console.log('>>>>>>>>>>>>coinase request \n\n ',post_data);
+            const payment_detail = post_data.event.data.payments[0];
+            const detail = payment_detail.value.crypto;
+            const crypto = detail.currency;
+            const amount = detail.amount;
+            console.log(">>>>>>>>>>>>>>>>>>> transction Received <<<<<<<<<<<< :: ", amount, crypto);
+            if (crypto == 'ETH'){
+                eth.forwardEth()
+            }
+            else if( crypto == 'DAI'){
+                erc.dai.forwardDai();
+    
+            }else if(crypto == 'USDT'){
+                erc.usdc.forwardUsdc();
+            }else{
+              return res.json({
+                  'status' : HTTPStatus.OK
+                });              
+            }
+        }catch(error){
+            console.log(error);
         }
-        else if( crypto == 'DAI'){
-            erc.dai.forwardDai();
 
-        }else if(crypto == 'USDT'){
-            erc.usdc.forwardUsdc();
-        }else{
-          return res.json({
-              'status' : HTTPStatus.OK
-            });              
-        }
     }
 
     module.exports =  async (req, res, next) => {
